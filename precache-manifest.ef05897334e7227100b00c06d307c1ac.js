ffaf741f7fac4863630b2560dafcb42d823c4eef
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-04-20 00:40:57 +0300

    Updates

---
+++ precache-manifest.ef05897334e7227100b00c06d307c1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c14a751f24433eb605ed300ed4b024",
+    "url": "/react-app-calculate-water/static/media/3.b7c14a75.jpeg"
+  },
+  {
+    "revision": "9d5af051ff5d26084f7f",
+    "url": "/react-app-calculate-water/static/js/runtime~main.2325907f.js"
+  },
+  {
+    "revision": "90215559aa0a263c23e0",
+    "url": "/react-app-calculate-water/static/js/main.bd973603.chunk.js"
+  },
+  {
+    "revision": "c9d7da6f1bcbc75cca99",
+    "url": "/react-app-calculate-water/static/js/2.85a11bac.chunk.js"
+  },
+  {
+    "revision": "90215559aa0a263c23e0",
+    "url": "/react-app-calculate-water/static/css/main.c88aa8c7.chunk.css"
+  },
+  {
+    "revision": "c9d7da6f1bcbc75cca99",
+    "url": "/react-app-calculate-water/static/css/2.10a71526.chunk.css"
+  },
+  {
+    "revision": "ce67efbf8652fa056e835ec93e3dd4f3",
+    "url": "/react-app-calculate-water/index.html"
+  }
+];
